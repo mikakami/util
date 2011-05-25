021837a5aaf5bc6a20887618454f19f88744139e
Kami <admin@softlabs.ws>
2011-05-25 13:13:50 +1100

    initial commit. will place all needed constants here (for them to be in one place)

---
+++ src/main/java/ws/softlabs/lib/util/client/Constants.java
@@ -0,0 +1,7 @@
+package ws.softlabs.lib.util.client;
+
+public class Constants {
+
+	public static final int dayOffset = 4;
+	public static final long oneDay = 24 * 60 * 60 * 1000; // msec in 1 day
+}
